5f57b4cb0a7d40725af40369fdcc0d605d3c667c
prvprdk <prvprdk@gmail.com>
2024-05-29 15:54:27 +0500

    add

---
+++ src/GFG.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class GFG {
+
+    public static void main(String[] args) {
+
+        //
+        permutation("abc", "");
+        //Heap
+        permutationHeap(new Number[]{1, 2, 3}, 3);
+        //shift array to left
+        permutationShift(new int[]{0, 1, 2});
+    }
+
+
+    static void permutation(String str, String ans) {
+
+        if (str.isEmpty()) {
+            System.out.println(ans + " ");
+            return;
+        }
+        IntStream.range(0, str.length()).forEach(i -> {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            permutation(ros, ans + ch);
+        });
+    }
+
+    static void permutationHeap(Number[] array, int n) {
+
+        if (n == 1) System.out.println(Arrays.toString(array));
+        else {
+
+            IntStream.range(0, n - 1).forEach(i -> {
+                permutationHeap(array, n - 1);
+
+                if (n % 2 == 0) swap(array, n - 1, i);
+                else swap(array, 0, n - 1);
+
+                System.out.println(Arrays.toString(array));
+
+            });
+        }
+    }
+
+    static void permutationShift(int[] array) {
+
+        int key = array.length - 1;
+        int ch = key;
+
+        System.out.println(Arrays.toString(array));
+
+        while (key > 0){
+            shift(array, key);
+            if (array[key] != key){
+                System.out.println(Arrays.toString(array));
+                key = ch;
+
+            }else key--;
+
+        }
+
+
+    }
+
+    static void swap(Number[] array, int a, int b) {
+        Number temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    static void shift(int[] array, int key) {
+        int temp = array[0];
+
+        IntStream.range(0, key).forEach(i -> {
+            array[i] = array[i + 1];
+        });
+        array[key] = temp;
+
+    }
+
+
+}
---
+++ src/GameTask.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class GameTask {
+
+    public static void main(String[] args) {
+
+        // task platform game
+        char[] str = new char[]{'L', 'R', 'F', 'F', 'L', 'R'};
+
+        List<String> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        IntStream.range(0, str.length).forEach(i -> {
+
+            char[] ch = str.clone();
+
+            switch (str[i]) {
+                case 'L':
+                    swap(ch, i, 'F', list);
+                    swap(ch, i, 'R', list);
+                   break;
+                case 'R':
+                    swap(ch, i, 'L', list);
+                    swap(ch, i, 'F', list);
+                    break;
+                case 'F':
+                    swap(ch, i, 'R', list);
+                    swap(ch, i, 'L', list);
+                    break;
+            }
+            ch = str.clone();
+
+        });
+
+        for (String s : list) {
+            int position = 0;
+            boolean isRight = true;
+
+            for (int x = 0; x < s.length(); x++) {
+
+                if (s.charAt(x) == 'L') isRight = false;
+                if (s.charAt(x) == 'R') isRight = true;
+
+                if (s.charAt(x) == 'F' && isRight) {
+                    position++;
+                    set.add(position);
+                }
+                if (s.charAt(x) == 'F' && !isRight) {
+                    position--;
+                    set.add(position);
+                }
+
+            }
+        }
+        System.out.printf("number of different positions: %d", set.size());
+    }
+
+   static void swap (char [] array, int index, char ch, List<String> list){
+        array[index] = ch;
+        list.add(Arrays.toString(array));
+    }
+}
